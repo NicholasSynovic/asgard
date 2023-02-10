85a58a9725c55e8258e86e768e9b6e99436d81f0
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-02-10 01:29:43 -0600

    Add ansible for clients

---
+++ clients/installAnsible.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove --purge -y
+
+ls /bin > defaultSoftware.txt
+
+sudo apt install software-properties-common -y
+ add-sudo apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+
+ansible-galaxy collection install community.general
--- clients/setupNFS_client.bash
+++ clients/setupNFS_client.bash
@@ -7,17 +7,6 @@ serverIP=$1
 serverFolderPath="/mnt/asgard"
 clientFolderPath="/mnt/asgard_client"
 
-sudo apt remove nfs-common --purge -y
-
-sudo apt update
-sudo apt upgrade -y
-sudo apt autoremove --purge -y
-
-sudo apt install nfs-common -y
-
-sudo rm -r $clientFolderPath
-sudo mkdir $clientFolderPath
-
 # Connect to NFS server at mount point
 # sudo mount $serverIP:$serverFolderPath $clientFolderPath
 sudo tee -a "${serverIP}:${serverFolderPath}	${clientFolderPath}	nfs	auto	0 0" /etc/fstab
