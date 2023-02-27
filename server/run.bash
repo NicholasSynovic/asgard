b20697cbed14c526c0f7ca6df0a23df1f7a71703
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-02-27 13:36:16 -0600

    Update runner script

--- server/run.bash
+++ server/run.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+sudo apt --fix-broken install -y
 sudo sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove --purge -y
+sudo apt --fix-broken install -y
 
-ls /bin > defaultSoftware_${date +%s}.txt
+ls /bin > defaultSoftware.txt
 
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+sudo apt --fix-broken install -y
 
 ansible-galaxy collection install community.general
 
-ansible-playbook -i inventory.ini playbook.yaml
+ansible-playbook -K -i inventory.ini playbook.yml
